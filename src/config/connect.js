5d44eeadb840060d14cd551bec307f8801b2815e
huytrinh102-prog <huytrinh102@gmail.com>
2026-05-07 14:56:34 +0900

    Use Railway MySQL env

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("jwt", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: Number(process.env.MYSQLPORT || 3306),
+    dialect: "mysql",
+    logging: false,
+  },
+);
 const connection = async () => {
   try {
     await sequelize.authenticate();
@@ -12,4 +19,5 @@ const connection = async () => {
     console.error("Unable to connect to the database:", error);
   }
 };
+
 export default connection;
