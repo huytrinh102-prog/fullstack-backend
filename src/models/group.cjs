91324e8b595038f89bc80ff7b927ecc4634eb6bd
huytrinh102-prog <huytrinh102@gmail.com>
2026-05-11 14:48:51 +0900

    Fix Group table name casing

--- src/models/group.cjs
+++ src/models/group.cjs
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "Group",
-      tableName: "group",
+      tableName: "Group",
     },
   );
   return Group;
